9aa335981f4cacfa95ef2362824f3497e1bc93c1
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-02-20 22:12:30 -0300

    feat: add models

---
+++ lib/models/category.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Category {
+  final String name;
+  final List<String> subcats;
+
+  Category({
+    required this.name,
+    required this.subcats,
+  });
+
+  Category copyWith({
+    String? name,
+    List<String>? subcats,
+  }) {
+    return Category(
+      name: name ?? this.name,
+      subcats: subcats ?? this.subcats,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'subcats': subcats,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      name: map['name'] as String,
+      subcats: List<String>.from(map['subcats'] as List<String>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Category.fromJson(String source) =>
+      Category.fromMap(json.decode(source) as Map<String, dynamic>);
+}
---
+++ lib/models/song.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+class Song {
+  final String? id;
+  final String title;
+  final List<String> categories;
+  final String? lyricsURL;
+  final String? chordsURL;
+  final bool favorite;
+
+  Song({
+    this.id,
+    required this.title,
+    required this.categories,
+    this.lyricsURL,
+    this.chordsURL,
+    this.favorite = false,
+  });
+
+  Song copyWith({
+    String? id,
+    String? title,
+    List<String>? categories,
+    String? lyricsURL,
+    String? chordsURL,
+    bool? favorite,
+  }) {
+    return Song(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      categories: categories ?? this.categories,
+      lyricsURL: lyricsURL ?? this.lyricsURL,
+      chordsURL: chordsURL ?? this.chordsURL,
+      favorite: favorite ?? this.favorite,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'categories': categories,
+      'lyricsURL': lyricsURL,
+      'chordsURL': chordsURL,
+      'favorite': favorite,
+    };
+  }
+
+  factory Song.fromMap(Map<String, dynamic> map) {
+    return Song(
+      id: map['id'] != null ? map['id'] as String : null,
+      title: map['title'] as String,
+      categories: List<String>.from(map['categories'] as List<String>),
+      lyricsURL: map['lyricsURL'] != null ? map['lyricsURL'] as String : null,
+      chordsURL: map['chordsURL'] != null ? map['chordsURL'] as String : null,
+      favorite: map['favorite'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Song.fromJson(String source) =>
+      Song.fromMap(json.decode(source) as Map<String, dynamic>);
+}
